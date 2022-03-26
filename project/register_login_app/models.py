aa5e462c398afe8fad0769bd7ddeccdc84f9e096
User <you@example.coavagyansamvel07@gmail.com>
2022-03-26 16:17:34 +0400

    add User model

--- project/register_login_app/models.py
+++ project/register_login_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.charField(max_length=20)
+    surname = models.charField(max_length=25)
+    email = models.charField(max_length=35)
+    password = models.charFiled(min_length=8)
+    confirmPassword = models.charFiled(min_length=8)
